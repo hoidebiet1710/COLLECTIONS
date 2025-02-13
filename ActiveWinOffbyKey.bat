9b3dda5f4ded7b69863689bc29469e870c5f4cba
hoidebiet1710 <64363302+hoidebiet1710@users.noreply.github.com>
2025-02-13 14:04:34 +0700

    Update ActiveWinOffbyKey.bat

--- ActiveWinOffbyKey.bat
+++ ActiveWinOffbyKey.bat
@@ -23,14 +23,14 @@ ECHO.
 set /p choice=Nhap lua chon (1-4): 
 ECHO.
 if %choice%==0 goto TheEnd
-if %choice%==1 goto winonl
-if %choice%==2 goto officeonl
+if %choice%==1 goto winonline
+if %choice%==2 goto officeonline
 if %choice%==3 goto winbyphone
 if %choice%==4 goto officebyphone
 
-:winonl
+:winonline
 cd %windir%\system32
-set /p key=Nhap key Windows: 
+set /p keyWin=Nhap key Windows: 
 cscript //nologo %windir%\system32\slmgr.vbs /rilc >nul 2>&1
 cscript //nologo %windir%\system32\slmgr.vbs /upk >nul 2>&1
 cscript //nologo %windir%\system32\slmgr.vbs /ckms >nul 2>&1
@@ -38,7 +38,7 @@ cscript //nologo %windir%\system32\slmgr.vbs /cpky >nul 2>&1
 sc config Winmgmt start=demand & net start Winmgmt
 sc config LicenseManager start= auto & net start LicenseManager
 sc config wuauserv start= auto & net start wuauserv
-cscript //nologo %windir%\system32\slmgr.vbs /ipk %key%
+cscript //nologo %windir%\system32\slmgr.vbs /ipk %keyWin%
 cscript //nologo %windir%\system32\slmgr.vbs /ato
 cscript //nologo %windir%\system32\slmgr.vbs /xpr
 cscript //nologo %windir%\system32\slmgr.vbs /dli
@@ -46,7 +46,7 @@ pause 2>nul & GOTO ACTIVEWINOFF
 
 :winbyphone
 cd %windir%\system32
-set /p key=Nhap key Windows: 
+set /p keyWin=Nhap key Windows: 
 cscript //nologo %windir%\system32\slmgr.vbs /rilc >nul 2>&1
 cscript //nologo %windir%\system32\slmgr.vbs /upk >nul 2>&1
 cscript //nologo %windir%\system32\slmgr.vbs /ckms >nul 2>&1
@@ -54,7 +54,7 @@ cscript //nologo %windir%\system32\slmgr.vbs /cpky >nul 2>&1
 sc config Winmgmt start=demand & net start Winmgmt
 sc config LicenseManager start= auto & net start LicenseManager
 sc config wuauserv start= auto & net start wuauserv
-cscript //nologo %windir%\system32\slmgr.vbs /ipk %key%
+cscript //nologo %windir%\system32\slmgr.vbs /ipk %keyWin%
 cls
 cscript //nologo %windir%\system32\slmgr.vbs /dti>C:\Win.txt & start C:\Win.txt
 start "" https://getcid.info/
@@ -66,28 +66,28 @@ cscript //nologo %windir%\system32\slmgr.vbs /dli
 pause 2>nul & GOTO ACTIVEWINOFF
 )
 
-:officeonl
-cd %windir%\system32
+:officeonline
+cd /d "%~dp0"
 for %%a in (4,5,6) do (if exist "%ProgramFiles%\Microsoft Office\Office1%%a\ospp.vbs" (cd /d "%ProgramFiles%\Microsoft Office\Office1%%a")
 If exist "%ProgramFiles% (x86)\Microsoft Office\Office1%%a\ospp.vbs" (cd /d "%ProgramFiles% (x86)\Microsoft Office\Office1%%a"))
-set /p key= Nhap Key Office: 
+set /p keyOffice= Nhap Key Office: 
 cls
 for %%a in (4,5,6) do (if exist "%ProgramFiles%\Microsoft Office\Office1%%a\ospp.vbs" (cd /d "%ProgramFiles%\Microsoft Office\Office1%%a")
 If exist "%ProgramFiles% (x86)\Microsoft Office\Office1%%a\ospp.vbs" (cd /d "%ProgramFiles% (x86)\Microsoft Office\Office1%%a"))&cls
-cscript OSPP.VBS /inpkey:%key%
+cscript OSPP.VBS /inpkey:%keyOffice%
 cscript OSPP.VBS /act & cscript OSPP.VBS /dstatus
 start Excel
 pause 2>nul & GOTO ACTIVEWINOFF
 )
 
 :officebyphone
-cd %windir%\system32
+cd /d "%~dp0"
 for %%a in (4,5,6) do (if exist "%ProgramFiles%\Microsoft Office\Office1%%a\ospp.vbs" (cd /d "%ProgramFiles%\Microsoft Office\Office1%%a")
 If exist "%ProgramFiles% (x86)\Microsoft Office\Office1%%a\ospp.vbs" (cd /d "%ProgramFiles% (x86)\Microsoft Office\Office1%%a"))
-set /p key= Nhap Key Office: 
+set /p keyOffice= Nhap Key Office: 
 for %%a in (4,5,6) do (if exist "%ProgramFiles%\Microsoft Office\Office1%%a\ospp.vbs" (cd /d "%ProgramFiles%\Microsoft Office\Office1%%a")
 If exist "%ProgramFiles% (x86)\Microsoft Office\Office1%%a\ospp.vbs" (cd /d "%ProgramFiles% (x86)\Microsoft Office\Office1%%a"))&cls
-cscript OSPP.VBS /inpkey:%key%
+cscript OSPP.VBS /inpkey:%keyOffice%
 cscript ospp.vbs /dinstid>C:\Office.txt & start C:\Office.txt
 start "" https://getcid.info/
 set /p CID= Nhap confirmation ID :
