ce2ddd2e4ad7f0a16f635bff878d41fc384dbc9b
hoidebiet1710 <64363302+hoidebiet1710@users.noreply.github.com>
2025-02-25 16:21:30 +0700

    Update Remove-OneDrive.bat

--- BloadwareRemoveUtility/Remove-OneDrive.bat
+++ BloadwareRemoveUtility/Remove-OneDrive.bat
@@ -3,52 +3,38 @@ CHCP 65001 >nul
 setlocal enabledelayedexpansion
 cd /d "%~dp0"
 
-echo Kill OneDrive process
-taskkill.exe /F /IM "OneDrive.exe"
-taskkill.exe /F /IM "explorer.exe"
-
-echo Remove OneDrive
-if exist "%systemroot%\System32\OneDriveSetup.exe" (
-    "%systemroot%\System32\OneDriveSetup.exe" /uninstall
+set x86="%ProgramFiles(x86)%\Microsoft OneDrive\OneDriveSetup.exe"
+set x64="%ProgramFiles%\Microsoft OneDrive\OneDriveSetup.exe"
+taskkill.exe /f /im OneDrive.exe
+if exist %x64% (
+%x64% /uninstall
+) else (
+%x86% /uninstall
 )
-if exist "%systemroot%\SysWOW64\OneDriveSetup.exe" (
-    "%systemroot%\SysWOW64\OneDriveSetup.exe" /uninstall
-)
-
-echo Removing OneDrive leftovers
-rmdir /S /Q "%localappdata%\Microsoft\OneDrive" 2>nul
-rmdir /S /Q "%programdata%\Microsoft OneDrive" 2>nul
-rmdir /S /Q "%systemdrive%\OneDriveTemp" 2>nul
-
-:: Check if directory is empty before removing
-dir /A /B "%userprofile%\OneDrive" | findstr . >nul
-if errorlevel 1 (
-    rmdir /S /Q "%userprofile%\OneDrive" 2>nul
-)
-
-echo Disable OneDrive via Group Policies
-reg add "HKLM\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\OneDrive" /v "DisableFileSyncNGSC" /t REG_DWORD /d 1 /f
-
-echo Remove OneDrive from explorer sidebar
-reg add "HKCR\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d 0 /f
-reg add "HKCR\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d 0 /f
-
-echo Removing run hook for new users
-reg load "hku\Default" "C:\Users\Default\NTUSER.DAT"
-reg delete "HKEY_USERS\Default\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDriveSetup" /f
-reg unload "hku\Default"
-
-echo Removing startmenu entry
-del /F /Q "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk" 2>nul
-
-echo Removing scheduled task
-schtasks /Delete /TN "OneDrive*" /F >nul 2>&1
-
-echo Restarting explorer
-start explorer.exe
-
-echo Waiting for explorer to complete loading
-timeout /T 5 /NOBREAK >nul
-
-echo OneDrive removal and disabling completed.
+rd "%USERPROFILE%\OneDrive" /Q /S
+rd "%systemroot%/Users/%username%/OneDrive" /Q /S
+rd "C:\OneDriveTemp" /Q /S
+rd "%LOCALAPPDATA%\Microsoft\OneDrive" /Q /S
+rd "%PROGRAMDATA%\Microsoft OneDrive" /Q /S
+TASKKILL.exe /F /IM OneDrive.exe /T 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\OneDrive" /v "DisableFileSyncNGSC" /t REG_DWORD /d 1 /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\OneDrive" /v "DisableLibrariesDefaultSaveToOneDrive" /t REG_DWORD /d 1 /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\OneDrive" /v "DisableMeteredNetworkFileSync" /t REG_DWORD /d 1 /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Onedrive" /v "DisableFileSyncNGSC" /t REG_DWORD /d 1 /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Onedrive" /v "DisableLibrariesDefaultSaveToOneDrive" /t REG_DWORD /d 1 /f 
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Onedrive" /v "DisableMeteredNetworkFileSync" /t REG_DWORD /d 1 /f 
+reg add "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d 0 /f 
+reg add "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d 0 /f 
+reg add "HKEY_CURRENT_USER\Software\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d 0 /f 
+reg add "HKEY_CURRENT_USER\Software\Classes\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /v "System.IsPinnedToNameSpaceTree" /t REG_DWORD /d 0 /f 
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "OneDrive" /f
+takeown /F %Windir%\SysWOW64\OneDriveSetup.exe
+icacls %Windir%\SysWOW64\OneDriveSetup.exe /grant Administradores:F
+del %Windir%\SysWOW64\OneDriveSetup.exe
+takeown /F %Windir%\SysWOW64\OneDriveSettingSyncProvider.dll
+icacls %Windir%\SysWOW64\OneDriveSettingSyncProvider.dll /grant Administradores:F
+del %Windir%\SysWOW64\OneDriveSettingSyncProvider.dll
+Del %AppData%\Microsoft\Windows\Start Menu\Programs\OneDrive.lnk 
+REG DELETE "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
+REG DELETE "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
 pause 2>nul & Exit /b
